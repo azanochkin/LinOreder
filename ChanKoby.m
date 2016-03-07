faef9e5f85a8dd1a94e13b2e77a82587061882d4
azanochkin <andyzanochkin@gmail.com>
2016-03-07 11:32:54 +0300

    Removed unused functions

--- ChanKoby.m
+++
@@ -1,7 +0,0 @@
-function [ newRankVec, kemenyDist ] = ChanKoby(rankVec, lossMat, optimFnc, nIter)
-    newRankVec = rankVec;
-    for i = 1:nIter
-        [newRankVec,kemenyDist] = optimFnc(newRankVec(end:-1:1),lossMat);
-    end
-end
-
