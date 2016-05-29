35388059257bb7c2e56b39c558fb0a1cfd9557a8
azanochkin <andyzanochkin@gmail.com>
2016-05-30 00:17:07 +0300

    Удалены лишние файлы

--- Stats/regulPlot.m
+++
@@ -1,8 +0,0 @@
-load('ToMaratReg.mat')
-scatterRanks(consRegRankMat(:,1:2));
-xlabel('Оптимальная медиана')
-ylabel('Около-оптимальная медиана')
-%%
-scatterRanks(consRegRankMat(:,[1,3]));
-xlabel('Оптимальная медиана')
-ylabel('Неоптимальная медиана')
